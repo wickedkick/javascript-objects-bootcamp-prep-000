4d6a5b5bc8ff0514e47f6672ed4055cc69d3042b
Imran Iqbal <imran1337@gmail.com>
2019-08-11 11:00:39 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,3 +5,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return updatePlaylist;
 }
 
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+  removeFromPlaylist;
+}
