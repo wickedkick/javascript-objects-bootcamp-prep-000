a452357afce426256e186c7a50d502bb5aae443e
Imran Iqbal <imran1337@gmail.com>
2019-08-11 10:53:52 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { RKelly: 'Remix to Ignition' }
 
 function updatePlaylist(playlist, artist, songTitle) {
-  Object.assign({}, playlist, { artist, songTitle })
+  Object.assign({}, playlist, { [artist]: songTitle })
   return updatePlaylist
 }
