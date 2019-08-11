bb4d69ee4cc086aa56d2cca88569d61d1598595c
Imran Iqbal <imran1337@gmail.com>
2019-08-11 11:03:27 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = { RKelly: 'Remix to Ignition' }
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return updatePlaylist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+  return removeFromPlaylist;
+}
